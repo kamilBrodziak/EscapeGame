393ee3177d5433cf25b6a01c60bc49f5d06fbd5f
Kamil <kamil98brodziak@gmail.com>
2020-06-28 18:10:01 +0200

    Add LevelGamesController and LevelGamesView with one minigame - hotncold

---
+++ Controller/LevelGamesController.cs
@@ -0,0 +1,50 @@
+﻿
+using EscapeGame.View;
+using System;
+
+namespace EscapeGame.Controller {
+    class LevelGamesController {
+        private LevelGamesView levelGamesView { get; set; }
+
+        public LevelGamesController() {
+            levelGamesView = new LevelGamesView();
+        }
+        public bool RunMiniGame(int level) {
+            switch(level) {
+                case 1:
+                    return TicTacToe();
+                case 2:
+                    return Hangman();
+                case 0:
+                default:
+                    return HotNCold();
+            }
+        }
+
+        public bool HotNCold() {
+            levelGamesView.PrintHotNColdEntrance();
+            Random rnd = new Random();
+            int number = rnd.Next(0, 100);
+            for(int i = 1; i <= 5; ++i) {
+                if(levelGamesView.GetUserInput() != $"{number}") {
+                    levelGamesView.PrintHotNColdWrongAnswer(5 - i);
+                } else {
+                    levelGamesView.PrintHotNColdResult(true);
+                    return true;
+                }
+            }
+            levelGamesView.PrintHotNColdResult(false);
+            return false;
+        }
+
+        public bool TicTacToe() {
+            int[] moves = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //levelGamesView.Print
+            return false;
+        }
+
+        public bool Hangman() {
+            return false;
+        }
+    }
+}
---
+++ View/LevelGamesView.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EscapeGame.View {
+    public class LevelGamesView {
+
+        public string GetUserInput() {
+            return Console.ReadLine();
+        }
+
+        public ConsoleKey GetUserKeyInput() {
+            return Console.ReadKey().Key;
+        }
+
+        //
+        // HOT N COLD MINI GAME
+        //
+        public void PrintHotNColdEntrance() {
+            Console.Clear();
+            Console.WriteLine("Hello, I am nameless AI who trapped you here.");
+            Console.WriteLine("I want to test, if you are worthy of living, HA HA HA.");
+            Console.WriteLine("Let's play a game! I am thinking about number from 0 to 100.");
+            Console.WriteLine("What number is it? You have 5 tries to guess");
+        }
+
+        public void PrintHotNColdWrongAnswer(int triesLeft) {
+            Console.WriteLine($"Wrong! Try again, you have {triesLeft} tries left");
+        }
+
+        public void PrintHotNColdResult(bool playerWon) {
+            System.Threading.Thread.Sleep(4000);
+            if (playerWon) {
+                Console.WriteLine($"You won! Congratulations. We will meet again, now you can pass to another level.");
+
+            } else {
+                Console.WriteLine("Oh my dear, you lost! But don't worry, you can try again, taste my mercy, HA HA HA!");
+            }
+        }
+
+        //
+        // TIC TAC TOE MINI GAME
+        //
+
+        public void PrintTicTacToeEntrance() {
+            Console.WriteLine("Hello again my dear!");
+            Console.WriteLine("You are doing really really great!");
+            Console.WriteLine("But you are sooooo slow.. I am bored again!");
+            Console.WriteLine("Let's Play another game! You start!");
+            Console.WriteLine("Use left/right arrow to choose your move and hit enter when you want to make that move!");
+        }
+    }
+}
